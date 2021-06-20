446faf00e27ea355e0db9684ff5618e85a9c812b
nikita.lankin <valnurray@gmail.com>
2021-06-20 23:36:04 +0300

    Initial

---
+++ src/main/java/com/lankin/rest/demo/model/Article.java
@@ -0,0 +1,61 @@
+package com.lankin.rest.demo.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "article")
+public class Article {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "author")
+    private String author;
+
+    public Article() {
+
+    }
+
+    public Article(Long id, String title, String author) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    @Override
+    public String toString() {
+        return "Article{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                '}';
+    }
+}
